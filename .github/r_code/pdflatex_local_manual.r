c7ca18d30a80cd1a191190f3c6bd554215772f98
e-perl-NOAA <elizabeth.gugliotti@noaa.gov>
2024-06-21 10:39:57 -0400

    alt text; subscript fixes; SB to SSB; equation notation; virgin spawning biomass; unit abbreviations; add mg to glossary

--- .github/r_code/pdflatex_local_manual.r
+++ .github/r_code/pdflatex_local_manual.r
@@ -1,31 +1,56 @@
 #' Comment out lines that prevent pdf from being generated locally and produce pdf
-#' @description Reads the tex file and comments out lines that prevent pdf from being
-#'   generated locally and  produces pdf using tinytex::pdflatex() and then changes 
-#'   the tex file back.
-#' @param manual_file Name of the manual tex file. Default is "SS330_User_Manual.tex"
-#'  so you don't need to put one in there.
+#' 
+#' Reads the tex file and comments out lines that prevent pdf from being
+#' generated locally and  produces pdf using tinytex::pdflatex() and then changes 
+#' the tex file back.
+#' 
+#' @param manual_file Name of the manual tex file. Default is "SS330_User_Manual.tex" 
+#' with the assumption that this file is in your working directory.
+#' @param dir Directory if "SS330_User_Manual.tex" is not in your working directory.
 #' @author Elizabeth F. Perl
-
-pdflatex_local_manual <- function(manual_file = "SS330_User_Manual.tex")
+#' @return Results from tinytex::pdflatex().
+#' @examples 
+#' pdflatex_local_manual()
+pdflatex_local_manual <- function(manual_file = "SS330_User_Manual.tex", dir = NULL)
 {
-    manual <- readLines(file.path(getwd(),"SS330_User_Manual.tex"))
+    if(is.null(dir)){
+        dir <- getwd()
+    } else {
+        dir <- dir
+    }
+    manual <- readLines(file.path(dir,"SS330_User_Manual.tex"))
     manual[3] <- gsub("\\\\","%\\\\",manual[3])
     manual[4] <- gsub("\\\\","%\\\\",manual[4])
-    manual[6] <- gsub("\\\\","%\\\\",manual[6])
+    manual[5] <- gsub("\\\\","%\\\\",manual[5])
     manual[7] <- gsub("\\\\","%\\\\",manual[7])
     manual[8] <- gsub("\\\\","%\\\\",manual[8])
     manual[9] <- gsub("\\\\","%\\\\",manual[9])
+    manual[10] <- gsub("\\\\","%\\\\",manual[10])
     writeLines(manual, "SS330_User_Manual.tex")
 
-    result <- tinytex::pdflatex("SS330_User_Manual.tex")
+    result <- tryCatch(
+        expr = {
+            message(tinytex::pdflatex("SS330_User_Manual.tex"))
+            message("Successfully created SS330_User_Manual.pdf")
+        },
+        error = function(e){
+            message('Caught an error!')
+            print(e)
+        },
+        warning = function(w){
+            message('Caught a warning!')
+            print(w)
+        }
+    )
 
-    manual <- readLines(file.path(getwd(),"SS330_User_Manual.tex"))
+    manual <- readLines(file.path(dir,"SS330_User_Manual.tex"))
     manual[3] <- gsub("%\\\\", "\\\\",manual[3])
     manual[4] <- gsub("%\\\\", "\\\\",manual[4])
-    manual[6] <- gsub("%\\\\", "\\\\",manual[6])
+    manual[5] <- gsub("%\\\\", "\\\\",manual[5])
     manual[7] <- gsub("%\\\\", "\\\\",manual[7])
     manual[8] <- gsub("%\\\\", "\\\\",manual[8])
-    manual[9] <- gsub("%\\\\", "\\\\",manual[9])
+    manual[9] <- gsub("\\\\","%\\\\",manual[9])
+    manual[10] <- gsub("%\\\\", "\\\\",manual[10])
     writeLines(manual, "SS330_User_Manual.tex")
 
     return(result)
