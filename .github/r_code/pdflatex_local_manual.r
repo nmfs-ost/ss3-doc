a1039f36fdef798765911bcf68c8ef596e96f684
e-perl-NOAA <elizabeth.gugliotti@noaa.gov>
2024-06-25 13:15:44 -0400

    pdflatex_local_manual description and parameter changes

--- .github/r_code/pdflatex_local_manual.r
+++ .github/r_code/pdflatex_local_manual.r
@@ -1,13 +1,15 @@
 #' Comment out lines that prevent pdf from being generated locally and produce pdf
 #' 
 #' Reads the tex file and comments out lines that prevent pdf from being
-#' generated locally and  produces pdf using tinytex::pdflatex() and then changes 
-#' the tex file back.
+#' generated locally and produces pdf using tinytex::pdflatex() and then changes 
+#' the tex file back. Please see issue #135 in the ss3-doc repository 
+#' (https://github.com/nmfs-ost/ss3-doc/issues/135) for more information on the 
+#' lines that prevent the pdf from being generated locally.
 #' 
-#' @param manual_file Name of the manual tex file. Default is "SS330_User_Manual.tex" 
-#' with the assumption that this file is in your working directory.
-#' @param dir Directory if "SS330_User_Manual.tex" is not in your working directory, 
-#' otherwise the function uses getwd() ad the default.
+#' @param manual_file A string providing the name of the manual tex file. The default is
+#' "SS330_User_Manual.tex" with the assumption that this file is in your working directory.
+#' @param dir A string providing the path to the directory where `manual_file` is located. 
+#' The default is [getwd()].
 #' @author Elizabeth F. Perl
 #' @return Results from tinytex::pdflatex().
 #' @examples 
@@ -38,7 +40,6 @@ pdflatex_local_manual <- function(manual_file = "SS330_User_Manual.tex", dir = g
         }
     )
 
-    manual <- readLines(file.path(dir,"SS330_User_Manual.tex"))
     manual[3] <- gsub("%\\\\", "\\\\",manual[3])
     manual[4] <- gsub("%\\\\", "\\\\",manual[4])
     manual[6] <- gsub("%\\\\", "\\\\",manual[6])
